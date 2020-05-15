e64ddc86e6503bcc0061448a8611da245e3d4ea7
lukemillerq <45546069+lukemillerq@users.noreply.github.com>
2020-05-14 22:34:29 -0700

    Add files via upload

--- ps2/src/bayesianreg/doubledescent.py
+++ ps2/src/bayesianreg/doubledescent.py
@@ -25,6 +25,15 @@ def ridge_regression(train_path, validation_path):
         val_err: List of validation errors for different scaling factors of lambda in scale_list.
     """
     # *** START CODE HERE ***
+    X, y = util.load_dataset(train_path)
+    x_valid, y_valid = util.load_dataset(validation_path)
+    opt = (sigma ** 2)/(eta ** 2)
+    val_err = []
+    for scale in scale_list:
+        theta_hat = np.linalg.pinv(X.T.dot(X) + scale * opt * np.identity(X.shape[1])).dot(X.T.dot(y))
+        preds = x_valid.dot(theta_hat)
+        err = np.sum(np.square(preds - y_valid))/x_valid.shape[0]
+        val_err.append(err)
     # *** END CODE HERE
     return val_err
 
